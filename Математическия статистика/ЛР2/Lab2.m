08756ade4114d65ae409f7fc8c5cf6d9d6cc474a
Inlucker <Arseny707@mail.ru>
2022-05-23 15:53:28 +0300

    defended lab2 and added rk2 file for matstat

--- Математическия статистика/ЛР2/Lab2.m
+++ Математическия статистика/ЛР2/Lab2.m
@@ -2,14 +2,16 @@ close all
 clear
 clc
 X=[-13.40 -12.63 -13.65 -14.23 -13.39 -12.36 -13.52 -13.44 -13.87 -11.82 -12.01 -11.40 -13.02 -12.61 -13.06 -13.75 -13.55 -14.01 -11.75 -12.95 -12.59 -13.60 -12.76 -11.05 -13.15 -13.61 -11.73 -13.00 -12.66 -12.67 -12.60 -12.47 -13.52 -12.61 -11.93 -13.11 -13.22 -11.87 -13.44 -12.70 -11.78 -12.30 -12.89 -13.29 -12.48 -10.44 -12.55 -12.64 -12.03 -14.60 -14.56 -13.30 -11.32 -12.24 -11.17 -12.50 -13.25 -12.55 -12.85 -12.67 -12.41 -12.58 -12.10 -13.54 -12.69 -12.87 -12.71 -12.77 -13.30 -12.74 -12.73 -12.64 -12.18 -11.20 -12.40 -13.78 -13.71 -10.74 -11.89 -13.20 -11.31 -14.26 -10.38 -12.88 -11.39 -11.35 -12.55 -12.84 -10.25 -12.40 -14.01 -11.47 -13.14 -12.69 -11.92 -12.86 -13.06 -12.57 -13.63 -12.34 -12.84 -14.03 -13.34 -11.64 -13.58 -10.44 -11.37 -11.01 -13.80 -13.27 -12.32 -10.69 -12.92 -13.29 -12.58 -13.98 -11.46 -11.82 -12.33 -11.47];
-%X=[X, zeros(1,20)-16];
+% X=sort(X);
+% X=flip(X);
+% X=[X, zeros(1,20)-16];
 
 %Task1
 mu = get_mu(X);
 Ssqr = get_Ssqr(X);
 % sigmasqr = sum(power(X-mu, 2))/size(X, 2);
-% gamma = 0.9;
-gamma = input("Введите уровень доверия: ");
+gamma = 0.9;
+% gamma = input("Введите уровень доверия: ");
 dov_interval_mu = get_dov_interval_mu(X, 0.9);
 mu_lower = dov_interval_mu(1);
 mu_upper = dov_interval_mu(2);
@@ -31,8 +33,9 @@ mu_y = [];
 n_x = [];
 muN_y = [];
 n = size(X, 2);
+start_n = 10;
 % n = input("Введите максимальны объём выборки: ");
-for i=1:n
+for i=start_n:n
     tmp = get_dov_interval_mu_N(X, gamma, i);
     mu_lower_y = [mu_lower_y tmp(1)];
     mu_upper_y = [mu_upper_y tmp(2)];
@@ -59,7 +62,7 @@ sigma_y = [];
 n_x = [];
 sigmaN_y = [];
 % n = size(X, 2);
-for i=1:n
+for i=start_n:n
     tmp = get_dov_interval_sigma_N(X, gamma, i);
     sigma_lower_y = [sigma_lower_y tmp(1)];
     sigma_upper_y = [sigma_upper_y tmp(2)];
@@ -81,15 +84,25 @@ legend('Нижняя граница \sigma^2(x_n)','Вверхняя грани�
 hold off;
 % saveas(gcf,'g2.jpg');
 
+% R_mu = mu_upper_y - mu_lower_y;
+% R_sigma = sigma_upper_y - sigma_lower_y;
+% figure('Position', [780 200 560 420]);
+% hold on;
+% plot(n_x, R_mu);
+% plot(n_x, R_sigma);
+% grid;
+% legend('Размах дов. инт. для \mu','Размах дов. инт. для \sigma^2');
+% hold off;
+
 function mu = get_mu(X)
     mu = sum(X) / size(X, 2);
 end
 
-function sigmasqr = get_sigmasqr(X)
-    n = size(X, 2);
-    mu = get_mu(X);
-    sigmasqr = sum(power(X-mu, 2))/n;
-end
+% function sigmasqr = get_sigmasqr(X)
+%     n = size(X, 2);
+%     mu = get_mu(X);
+%     sigmasqr = sum(power(X-mu, 2))/n;
+% end
 
 function Ssqr = get_Ssqr(X)
     n = size(X, 2);
